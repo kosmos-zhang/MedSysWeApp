1e3f8d892eae716e485ec320837ec4de311b56cc
kosmos-zhang <kosmos_zhang@hotmail.com>
2017-01-09 22:57:14 +0800

    修改相关功能，只保留一个扫描按钮

--- src/pages/medicinekit/kit.js
+++ src/pages/medicinekit/kit.js
@@ -2,43 +2,20 @@ var initData = 'this is first line\nthis is second line'
 Page({
   data: {
     text: initData,
-    input_type: 0,
-    input_wrp_style: "flex-direction:row; ",
-    scan_wrp_style: "flex-direction:row; display:none;",
+    medinines: ["阿司匹林", "头孢克洛"],
   },
   onshow: function() {
     var appInstance = getApp()
     console.log(appInstance.globalData)
   },
-  showinput: function(e) {
-    if (this.data.input_type == 0) {
-        this.setData({
-            input_wrp_style : "flex-direction:row; display:none;",
-            scan_wrp_style : "flex-direction:row; ",
-            input_type: 1,
-        })
-    }
-    else {
-        this.setData({
-            input_wrp_style: "flex-direction:row; ",
-            scan_wrp_style: "flex-direction:row; display:none;",
-            input_type: 0,
-        })
-    }
-  },
   scanbarcode: function(e) {
     wx.scanCode({
       success: (res) => {
-        // verify the res code
+        this.data.medinines = this.data.medinines.concat([res.result])
+        this.setData({
+          medinines: this.data.medinines
+        })
       }
     })
-  },
-  drugreact: function(e) {
-  },
-  drugdesc: function(e) {
-  },
-  drugremind: function(e) {
-  },
-  drugdel: function(e) {
   },
 })
