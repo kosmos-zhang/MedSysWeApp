ef05ad1aa6848eeeb9fc2ed67cf654b8ff166cef
kosmos-zhang <kosmos_zhang@hotmail.com>
2017-01-10 22:20:58 +0800

    刷新滑动删除按钮

--- src/pages/medicinekit/kit.js
+++ src/pages/medicinekit/kit.js
@@ -1,12 +1,16 @@
-var initData = 'this is first line\nthis is second line'
+var app = getApp();
+var startX;
+var startY;
+var endX;
+var endY;
+var key;
+var maxRight = 60;
 Page({
   data: {
-    text: initData,
-    medinines: ["阿司匹林", "头孢克洛"],
+    cardTeams: [{"id":"aaaa", "name":"阿司匹林", "right":0, "startRight":0},{"id":"bbbb", "name":"阿莫西林颗粒", "right":0, "startRight":0}]
   },
   onshow: function() {
-    var appInstance = getApp()
-    console.log(appInstance.globalData)
+    console.log(app.globalData)
   },
   scanbarcode: function(e) {
     wx.scanCode({
@@ -18,4 +22,94 @@ Page({
       }
     })
   },
+  drawStart : function(e){
+     // console.log("drawStart");
+    var touch = e.touches[0];
+    startX = touch.clientX;
+    startY = touch.clientY;
+    var cardTeams = this.data.cardTeams;
+    for(var i in cardTeams){
+        var data = cardTeams[i];
+        data.startRight = data.right;
+    }
+    key = true;
+  },
+  drawEnd : function(e){
+    console.log("drawEnd");
+    var cardTeams = this.data.cardTeams;
+    for(var i in cardTeams){
+        var data = cardTeams[i];
+        if(data.right <= 100/2){
+            data.right = 0;
+        }else{
+            data.right = maxRight;
+        }
+    }
+    this.setData({
+        cardTeams:cardTeams
+    });
+  },
+  drawMove : function(e){
+    //console.log("drawMove");
+    var self = this;
+    var dataId = e.currentTarget.id;
+    var cardTeams = this.data.cardTeams;
+    if(key){
+        var touch = e.touches[0];
+        endX = touch.clientX;
+        endY = touch.clientY;
+        console.log("startX="+startX+" endX="+endX );
+        if(endX - startX == 0)
+           return ;
+        var res = cardTeams;
+        //从右往左
+        if((endX - startX) < 0){
+            for(var k in res){
+                var data = res[k];
+                if(res[k].id == dataId){
+                    var startRight = res[k].startRight;
+                    var change = startX - endX;
+                    startRight += change;
+                    if(startRight > maxRight)
+                        startRight = maxRight;
+                    res[k].right = startRight;
+                }
+            }
+         } else {//从左往右
+            for(var k in res){
+                var data = res[k];
+                if(res[k].id == dataId){
+                    var startRight = res[k].startRight;
+                    var change = endX - startX;
+                    startRight -= change;
+                    if(startRight < 0)
+                        startRight = 0;
+                    res[k].right = startRight ;
+                }
+            }
+         }
+         self.setData({
+            cardTeams:cardTeams
+         });
+      }
+  },
+  //删除item
+  delItem: function(e){
+    var dataId = e.target.dataset.id;
+    console.log("删除"+dataId);
+    var cardTeams = this.data.cardTeams;
+    var newCardTeams = []; 
+    for(var i in cardTeams){
+        var item = cardTeams[i];
+        if(item.id != dataId){
+          newCardTeams.push(item);
+        }
+    }
+    this.setData({
+        cardTeams:newCardTeams
+     });
+  },
+  onLoad: function () {
+    console.log('onLoad:'+app.globalData.domain)
+  }  
 })
